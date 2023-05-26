b12216a76d9fac1eec0742a0f19639d6e2f538b4
phmacr <mark.crisostomo@e-horizon.com>
2023-05-26 13:22:11 +0800

    Initial Commit

---
+++ EmployeeInformationManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmployeeInformationManagement
+//
+//  Created by phmacr on 5/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
